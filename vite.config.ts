b6a47604ff6c1a042f9363ffa07e05663ed704c8
Gabriella Margarida Ribeiro Soares <gabriellamargarida18@gmail.com>
2025-09-25 21:54:07 -0300

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/ufjf-dcc207-2025-3-a-atv02-gabideiji",
   plugins: [react()],
 })
